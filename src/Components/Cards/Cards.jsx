2cdcf465d091f92bb3dadea4d68c19fe62811913
stutisk <48529714+stutisk@users.noreply.github.com>
2023-02-23 04:07:42 +0530

    feat:added featured tournament cards

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import styles from "../Cards/Cards.module.css";
+
+const Cardsdata = [
+  {
+    id: 1,
+    game: "PUBG SOLO",
+    price: "3200",
+    description: "This is the first card",
+    color: "#ffc609",
+    shadow: "0 3px 8px 0 rgb(255 198 9 / 30%)",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1677104346/xxklyxsgqwczy7x6bb3b.jpg",
+  },
+  {
+    id: 2,
+    price: "3200",
+    game: "Free Fire DUO",
+    description: "This is the second card",
+    color: "#43f2ff",
+    shadow: "0 3px 8px 0 rgb(67 242 255 / 30%)",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1677104346/xxklyxsgqwczy7x6bb3b.jpg",
+  },
+  {
+    id: 3,
+    price: "3200",
+    game: "Call Of Duty",
+    description: "This is the third card",
+    color: "#09ffc4",
+    shadow: "0 3px 8px 0 rgb(9 255 196 / 30%)",
+    image:
+      "https://res.cloudinary.com/dvbw8xujd/image/upload/v1677104346/xxklyxsgqwczy7x6bb3b.jpg",
+  },
+];
+
+export const Cards = () => {
+  return (
+    <>
+      {Cardsdata.map((card) => (
+        <div className={styles.cardsWrapper}>
+          <div
+            style={{ backgroundColor: card.color, boxShadow: card.shadow }}
+            className={styles.cards}
+          >
+            <div>
+              <div className={styles.cardimage}>
+                <img src={card.image} width="100%" className="border-radius" />
+              </div>
+              <div className="flex gap size-10 padding2">
+                <div className="flex gap">
+                  <img src="https://res.cloudinary.com/dvbw8xujd/image/upload/c_scale,h_10,w_11/v1677104783/s1x1jp6nt9e234ntrz9w.svg" />
+                  <div className={styles.cardPrice}> ₹{card.price}</div>
+                </div>
+                <div>in Winnings</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import styles from "../Header/Header.module.css";
 
 import { RxHamburgerMenu, TfiWallet } from "../Icons/Icons";
+import { Main } from "../Main/Main";
 export const Header = () => {
   return (
     <section className={styles.mainwrapper}>
@@ -20,29 +21,31 @@ export const Header = () => {
       </section>
 
       <section className={styles.main}>
-        <div className={styles.maindetails}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam
-          molestiae maxime enim. Porro maiores harum quos ut minus aliquid,
-          laudantium recusandae dolor expedita perferendis hic minima dolorem
-          consequatur neque molestiae. Lorem ipsum dolor sit, amet consectetur
-          adip hic
-          minima dolorem consequatur neque molestiae. maxime enim. Porro maiores
-          harum quos ut minus aliquid, laudantium recusandae dolor expedita
-          perferendis hic minima dolorem consequatur neque molestiae. Lorem
-          ipsum dolor sit, amet consectetur adipisicing elit. Nam molestiae
-          maxime enim. Porro maiores harum quos ut minus aliquid, laudantium
-          recusandae dolor expedita perferendis hic minima dolorem consequatur
-          nequ
-          a
-        </div>
+        {/* <div className={styles.maindetails}> */}
+          <Main/>
+        {/* </div> */}
       </section>
       <section className={styles.footer}>
         <ul className={styles.menu}>
-            <li className="cursor no-list-style icons "><img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095115/jhvxin4tpyh0t2g2inf9.svg" /></li>
-            <li className="cursor no-list-style icons"><img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095365/urqd3qte58ir31zar1go.svg" /></li>
-            <li className="cursor no-list-style center-icon padding-top"><img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095578/tq48bp7xzsqodv9k6bk1.svg  "  className="center-img" sizes="2"/></li>
-            <li className="cursor no-list-style icons"><img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095365/bu2ryuxbcpkotrmqlwjb.svg" /></li>
-            <li className="cursor no-list-style icons"><img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095449/fciekeh077emwyeutcso.svg" /></li>
+          <li className="cursor no-list-style icons ">
+            <img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095115/jhvxin4tpyh0t2g2inf9.svg" />
+          </li>
+          <li className="cursor no-list-style icons">
+            <img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095365/urqd3qte58ir31zar1go.svg" />
+          </li>
+          <li className="cursor no-list-style center-icon padding-top">
+            <img
+              src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095578/tq48bp7xzsqodv9k6bk1.svg  "
+              className="center-img"
+              sizes="2"
+            />
+          </li>
+          <li className="cursor no-list-style icons">
+            <img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095365/bu2ryuxbcpkotrmqlwjb.svg" />
+          </li>
+          <li className="cursor no-list-style icons">
+            <img src="https://res.cloudinary.com/dvbw8xujd/image/upload/v1677095449/fciekeh077emwyeutcso.svg" />
+          </li>
         </ul>
       </section>
     </section>
